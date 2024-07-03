de9810df4191e6237138a487cfcb0d6bcac2746b
ichbintanvir <tanvirnoyon.bubt@gmail.com>
2024-07-03 15:24:10 +0600

    create a folder  pages, many components, prepare many page, create sepatare pages.

--- src/child-component/RenconChild.jsx
+++ src/child-component/RenconChild.jsx
@@ -4,9 +4,12 @@ const RenconChild = ({ eachData }) => {
     return (
         // <div className="relative w-[343px] md:w-[369px] h-[413px] md:h-[432px]">
         <div className="relative max-w-[343px] md:max-w-[369px]">
-            <div className="bg-customBlack">
-                <img className="" src={img} alt="" />
-                <p className="absolute text-white bottom-0 font-cinzel font-[400] text-[30px] p-[16px] text-white">{title}</p>
+            <div className="">
+                <img src={img} alt="" />
+                {/* <p className="absolute text-white bottom-0 font-cinzel font-[400] text-[30px] p-[16px] text-white bg-customBlack3">{title}</p> */}
+                <div className="absolute inset-x-0 bottom-0">
+                    <p className="text-white font-cinzel font-[400] text-[30px] p-[16px] text-white bg-customBlack3">{title}</p>
+                </div>
             </div>
         </div>
     )
--- src/components/Carusol.jsx
+++ src/components/Carusol.jsx
@@ -9,7 +9,7 @@ const Carusol = () => {
     return (
         <div className='bg-[#F7F7F4]'>
             <div className="container mx-auto grid justify-center items-center py-[40px] md:py-[120px]">
-                <h1 className="uppercase font-cinzel font-[400] text-[24px] md:text-[56px] text-center pb-[24px] md:pb-[48px]">our clients say {slide}</h1>
+                <h1 className="uppercase font-cinzel font-[400] text-[24px] md:text-[56px] text-center pb-[24px] md:pb-[48px]">our clients say</h1>
                 <div className="grid justify-center items-center">
                     {
                         slide === 0 ? <Slide2 /> : <Slide1 />
@@ -17,12 +17,12 @@ const Carusol = () => {
 
                     <div className="flex justify-center gap-[12px] pt-[24px] md:pt-[48px]">
                         {/* <button onClick={() => setSlide(slide - 1)} className={`${slide == 0}?'bg-btnBG':'bg-red-400'`} disabled={slide == 0}> */}
-                        <button onClick={() => setSlide(slide - 1)} className={`${slide === 0} ? 'cursor-pointer':'cursor-not-allowed'`} disabled={slide == 0}>
-                            <IoIosArrowRoundBack className="text-[#D37D0C] text-[42px] border border-[#D37D0C]" />
+                        <button onClick={() => setSlide(slide - 1)} className={`${slide === 0 ? 'cursor-not-allowed' : 'cursor-pointer'}`} disabled={slide == 0}>
+                            <IoIosArrowRoundBack className={`text-[#D37D0C] text-[42px] border border-[#D37D0C] ${slide === 0 ? 'bg-btnBG' : ''}`} />
                         </button>
                         {/* <button onClick={() => setSlide(slide + 1)} className={`${slide == 1}?bg-btnBG:bg-red-400`} disabled={slide == 1}> */}
-                        <button onClick={() => setSlide(slide + 1)} className={`${slide === 1}?'cursor-pointer':'cursor-not-allowed'`} disabled={slide == 1}>
-                            <IoIosArrowRoundForward className="text-[#D37D0C] text-[42px] border border-[#D37D0C]" />
+                        <button onClick={() => setSlide(slide + 1)} className={`${slide === 1 ? 'cursor-not-allowed' : 'cursor-pointer'}`} disabled={slide == 1}>
+                            <IoIosArrowRoundForward className={`text-[#D37D0C] text-[42px] border border-[#D37D0C] ${slide === 1 ? 'bg-btnBG' : ''}`} />
                         </button>
                     </div>
                 </div>
